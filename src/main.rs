c3c9cc221614c1ca1b9a09c4cc2c0ec9c03424d0
Lee Morgan <Morgan.LeeR@protonmail.com>
2020-12-22 19:40:58 +0300

    Read from a file. Edit some bit of the file. Write to another file.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+use std::fs::File;
+use std::io::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    let thing = match read_file() {
+        Ok(contents) => edit_string(&contents),
+        Err(e) => panic!(e)
+    };
+
+    match create_file(&thing){
+        Err(e) => panic!(e),
+        _ => ()
+    }
+}
+
+fn read_file() -> std::io::Result<String> {
+    let mut file = File::open("../../javascript/InventoryManagement/views/dashboardPage/dashboard.ejs")?;
+    let mut contents = String::new();
+    file.read_to_string(&mut contents)?;
+    Ok(contents)
+}
+
+fn create_file(contents: &String) -> std::io::Result<()> {
+    let mut file = File::create("something.html")?;
+    file.write(contents.as_bytes())?;
+    Ok(())
+}
+
+fn edit_string(contents: &String) -> String{
+    contents.replace("INGREDIENTS", "ИНГЕДИЕТЫ")
 }
