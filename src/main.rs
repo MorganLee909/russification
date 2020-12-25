af2400c719cb83499ea740abc3d73278bcf7724b
Lee Morgan <Morgan.LeeR@protonmail.com>
2020-12-25 13:41:15 +0300

    Finish changes on dashboard.ejs. Cards on the analytics page are not translated yet.

--- src/main.rs
+++ src/main.rs
@@ -2,35 +2,32 @@ use std::fs::File;
 use std::io::prelude::*;
 
 fn main() {
-    let folder_location: String = "../../javascript/InventoryManagement".to_string();
-    dashboard_ejs(folder_location + "/views/dashboardPage/dashboard.ejs");
-    let folder_location: String = "../../javascript/InventoryManagement".to_string();
-    shared_css(folder_location + "/views/shared/shared.css");
-    let folder_location: String = "../../javascript/InventoryManagement".to_string();
-    dashboard_css(folder_location + "/views/dashboardPage/dashboard.css");
-    let folder_location: String = "../../javascript/InventoryManagement".to_string();
-    sidebars_css(folder_location + "/views/dashboardPage/sidebars.css");
+    let folder_location: &str = "../../javascript/InventoryManagement";
+
+    let mut file_location: &str = format!("{}{}", folder_location, "/views/dashboardPage/dashboard.ejs");
+    dashboard_ejs(file_location);
 }
 
-fn read_file(file: &String) -> std::io::Result<String> {
+fn read_file(file: &str) -> std::io::Result<String> {
     let mut file = File::open(file)?;
     let mut contents = String::new();
     file.read_to_string(&mut contents)?;
     Ok(contents)
 }
 
-fn write_file(file: &String, contents: String) -> std::io::Result<()> {
+fn write_file(file: &str, contents: String) -> std::io::Result<()> {
     let mut file = File::create(file)?;
     file.write(contents.as_bytes())?;
     Ok(())
 }
 
-fn dashboard_ejs(file: String) {
-    let mut contents = match read_file(&file) {
+fn dashboard_ejs(file: &str) {
+    let mut contents = match read_file(file) {
         Ok(contents) => contents,
         Err(e) => panic!(e)
     };
 
+    // TODO: get translations for the the cards on the analytics page
     contents = contents.replace("DASHBOARD", "ГЛАВНАЯ");
     contents = contents.replace("INGREDIENTS", "ИНГРЕДИЕНТЫ");
     contents = contents.replace("RECIPE BOOK", "КНИГА РЕЦЕПТОВ");
@@ -47,50 +44,9 @@ fn dashboard_ejs(file: String) {
     contents = contents.replace("RECIPES", "РЕЦЕПТЫ");
     contents = contents.replace("Date Range:", "Диапазон Дат:");
     contents = contents.replace("SUBMIT", "РАЗМЕСТИТЬ");
-
-    match write_file(&file, contents) {
-        Err(e) => panic!(e),
-        _ => ()
-    };
-}
-
-fn shared_css(file: String) {
-    let mut contents = match read_file(&file) {
-        Ok(contents) => contents,
-        Err(e) => panic!(e)
-    };
-
-    contents = contents.replace("green", "rgb(0, 27, 45)");
-
-    match write_file(&file, contents) {
-        Err(e) => panic!(e),
-        _ => ()
-    };
-}
-
-fn dashboard_css(file: String) {
-    let mut contents = match read_file(&file) {
-        Ok(contents) => contents,
-        Err(e) => panic!(e)
-    };
-
-    contents = contents.replace("green", "rgb(0, 27, 45)");
-
-    match write_file(&file, contents) {
-        Err(e) => panic!(e),
-        _ => ()
-    };
-}
-
-fn sidebars_css(file: String) {
-    let mut contents = match read_file(&file) {
-        Ok(contents) => contents,
-        Err(e) => panic!(e)
-    };
-
-    contents = contents.replace("green", "rgb(0, 27, 45)");
-
-    match write_file(&file, contents) {
+    contents = contents.replace("FILTER", "ФИЛЬТРОВАТЬ");
+    
+    match write_file(file, contents) {
         Err(e) => panic!(e),
         _ => ()
     };
