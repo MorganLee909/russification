df6e1d45c96374f1cc2c4fed25cc69b809f5616b
Lee Morgan <Morgan.LeeR@protonmail.com>
2020-12-25 13:14:45 +0300

    Update functions to work nicely with multiple files. Wrote some more code to edit different pages of the code and some of the translations.

--- src/main.rs
+++ src/main.rs
@@ -2,30 +2,96 @@ use std::fs::File;
 use std::io::prelude::*;
 
 fn main() {
-    let thing = match read_file() {
-        Ok(contents) => edit_string(&contents),
-        Err(e) => panic!(e)
-    };
-
-    match create_file(&thing){
-        Err(e) => panic!(e),
-        _ => ()
-    }
+    let folder_location: String = "../../javascript/InventoryManagement".to_string();
+    dashboard_ejs(folder_location + "/views/dashboardPage/dashboard.ejs");
+    let folder_location: String = "../../javascript/InventoryManagement".to_string();
+    shared_css(folder_location + "/views/shared/shared.css");
+    let folder_location: String = "../../javascript/InventoryManagement".to_string();
+    dashboard_css(folder_location + "/views/dashboardPage/dashboard.css");
+    let folder_location: String = "../../javascript/InventoryManagement".to_string();
+    sidebars_css(folder_location + "/views/dashboardPage/sidebars.css");
 }
 
-fn read_file() -> std::io::Result<String> {
-    let mut file = File::open("../../javascript/InventoryManagement/views/dashboardPage/dashboard.ejs")?;
+fn read_file(file: &String) -> std::io::Result<String> {
+    let mut file = File::open(file)?;
     let mut contents = String::new();
     file.read_to_string(&mut contents)?;
     Ok(contents)
 }
 
-fn create_file(contents: &String) -> std::io::Result<()> {
-    let mut file = File::create("something.html")?;
+fn write_file(file: &String, contents: String) -> std::io::Result<()> {
+    let mut file = File::create(file)?;
     file.write(contents.as_bytes())?;
     Ok(())
 }
 
-fn edit_string(contents: &String) -> String{
-    contents.replace("INGREDIENTS", "ИНГЕДИЕТЫ")
+fn dashboard_ejs(file: String) {
+    let mut contents = match read_file(&file) {
+        Ok(contents) => contents,
+        Err(e) => panic!(e)
+    };
+
+    contents = contents.replace("DASHBOARD", "ГЛАВНАЯ");
+    contents = contents.replace("INGREDIENTS", "ИНГРЕДИЕНТЫ");
+    contents = contents.replace("RECIPE BOOK", "КНИГА РЕЦЕПТОВ");
+    contents = contents.replace("ANALYTICS", "АНАЛИТИКА");
+    contents = contents.replace("ORDERS", "ЗАКАЗЫ");
+    contents = contents.replace("TRANSACTIONS", "ТРАНЗАКЦИИ");
+    contents = contents.replace("LOGOUT", "ВЫХОД");
+    contents = contents.replace("INGREDIENT INVENTORY", "ИНГРЕДИЕНТЫ");
+    contents = contents.replace("Total Revenue (month)", "Общий доход (месяц)");
+    contents = contents.replace("Inventory Check", "Проверить Ингредиеты");
+    contents = contents.replace(">Update", ">ОБНОВИТЬ");
+    contents = contents.replace("UPDATE", "ОБНОВИТЬ");
+    contents = contents.replace("NEW", "НОВЫЙ");
+    contents = contents.replace("RECIPES", "РЕЦЕПТЫ");
+    contents = contents.replace("Date Range:", "Диапазон Дат:");
+    contents = contents.replace("SUBMIT", "РАЗМЕСТИТЬ");
+
+    match write_file(&file, contents) {
+        Err(e) => panic!(e),
+        _ => ()
+    };
+}
+
+fn shared_css(file: String) {
+    let mut contents = match read_file(&file) {
+        Ok(contents) => contents,
+        Err(e) => panic!(e)
+    };
+
+    contents = contents.replace("green", "rgb(0, 27, 45)");
+
+    match write_file(&file, contents) {
+        Err(e) => panic!(e),
+        _ => ()
+    };
+}
+
+fn dashboard_css(file: String) {
+    let mut contents = match read_file(&file) {
+        Ok(contents) => contents,
+        Err(e) => panic!(e)
+    };
+
+    contents = contents.replace("green", "rgb(0, 27, 45)");
+
+    match write_file(&file, contents) {
+        Err(e) => panic!(e),
+        _ => ()
+    };
+}
+
+fn sidebars_css(file: String) {
+    let mut contents = match read_file(&file) {
+        Ok(contents) => contents,
+        Err(e) => panic!(e)
+    };
+
+    contents = contents.replace("green", "rgb(0, 27, 45)");
+
+    match write_file(&file, contents) {
+        Err(e) => panic!(e),
+        _ => ()
+    };
 }
