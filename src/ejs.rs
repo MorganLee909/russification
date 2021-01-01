3f2ab816a2aa6f4a127864740cea8e5ea475f5a5
Lee Morgan <Morgan.LeeR@protonmail.com>
2021-01-01 15:37:50 +0300

    Update the code structure in order to be easier to write and more modular. Update the files to reflect the new ejs structure. Fix typos in file names.

---
+++ src/ejs.rs
@@ -0,0 +1,215 @@
+pub struct Change<'a> {
+    pub location: String,
+    pub changes: Vec<&'a str>
+}
+
+pub fn change<'b>() -> Vec<Change<'b>>{
+    let folder_location: &str = "../../javascript/InventoryManagement/";
+
+    let menu_ejs = Change {
+        location: format!("{}{}", folder_location, "views/dashboardPage/ejs/menu.ejs"),
+        changes: vec![
+            "DASHBOARD", "ГЛАВНАЯ",
+            "INGREDIENTS", "ИНГРЕДИЕНТЫ",
+            "RECIPE BOOK", "КНИГА РЕЦЕПТОВ",
+            "ANALYTICS", "АНАЛИТИКА",
+            "ORDERS", "ЗАКАЗЫ",
+            "TRANSACTIONS", "ТРАНЗАКЦИИ",
+            "LOGOUT", "ВЫХОД",
+        ]
+    };
+
+    let analytics_ejs = Change {
+        location: format!("{}{}", folder_location, "views/dashboardPage/ejs/strands/analytics.ejs"),
+        changes: vec![
+            "SUBMIT", "РАЗМЕСТИТЬ",
+            "ANALYTICS", "АНАЛИТИКА",
+            "INGREDIENTS", "ИНГРЕДИЕНТЫ",
+            "RECIPES", "РЕЦЕПТЫ"
+        ]
+    };
+
+    let home_ejs = Change {
+        location: format!("{}{}", folder_location, "views/dashboardPage/ejs/strands/home.ejs"),
+        changes: vec![
+            "DASHBOARD", "ГЛАВНАЯ",
+            "Total Revenue (month)", "Общий доход (месяц)",
+            "Inventory Check", "Проверить Ингредиеты",
+            ">Update", ">ОБНОВИТЬ"
+        ]
+    };
+
+    let ingredients_ejs = Change {
+        location: format!("{}{}", folder_location, "views/dashboardPage/ejs/strands/ingredients.ejs"),
+        changes: vec![
+            "INGREDIENT INVENTORY", "ИНГРЕДИЕНТЫ",
+            "NEW", "НОВЫЙ"
+        ]
+    };
+
+    let orders_ejs = Change {
+        location: format!("{}{}", folder_location, "views/dashboardPage/ejs/strands/orders.ejs"),
+        changes: vec![
+            "ORDERS", "ЗАКАЗЫ",
+            "FILTER", "ФИЛЬТРОВАТЬ",
+            "NEW", "НОВЫЙ",
+            "CALCULATOR", "КАЛЬКУЛЯТОР"
+        ]
+    };
+
+    let recipe_book_ejs = Change {
+        location: format!("{}{}", folder_location, "views/dashboardPage/ejs/strands/recipeBook.ejs"),
+        changes: vec![
+            "RECIPE BOOK", "КНИГА РЕЦЕПТОВ",
+            "NEW", "НОВЫЙ"
+        ]
+    };
+
+    let transactions_ejs = Change {
+        location: format!("{}{}", folder_location, "views/dashboardPage/ejs/strands/transactions.ejs"),
+        changes: vec![
+            "TRANSACTIONS", "ТРАНЗАКЦИИ",
+            "NEW", "НОВЫЙ",
+            "FILTER", "ФИЛЬТРОВАТЬ"
+        ]
+    };
+
+    let edit_ingredient_ejs = Change {
+        location: format!("{}{}", folder_location, "views/dashboardPage/ejs/sidebars/editIngredient.ejs"),
+        changes: vec![
+            "NAME", "НАЗВАНИЕ",
+            "CATEGORY", "КАТЕГОРИЯ",
+            "MEASUREMENT UNIT", "ЕДИНИЦА ИЗМЕРЕНИЯ",
+            "SUBMIT", "РАЗМЕСТИТЬ"
+        ]
+    };
+
+    let edit_recipe_ejs = Change {
+        location: format!("{}{}", folder_location, "views/dashboardPage/ejs/sidebars/editRecipe.ejs"),
+        changes: vec![
+            "NAME", "НАЗВАНИЕ",
+            "INGREDIENTS", "ИНГРЕДИЕНТЫ",
+            "PRICE", "ЦЕНА",
+            "UPDATE", "ОБНОВИТЬ",
+            "CANCEL", "ОТМЕНИТЬ"
+        ]
+    };
+
+    let ingredient_details_ejs = Change {
+        location: format!("{}{}", folder_location, "views/dashboardPage/ejs/sidebars/ingredientDetails.ejs"),
+        changes: vec![
+            "CURRENT STOCK", "ТЕКУЩИЙ ЗАПАС",
+            "RECIPES", "РЕЦЕПТЫ"
+        ]
+    };
+
+    let new_ingredient_ejs = Change {
+        location: format!("{}{}", folder_location, "views/dashboardPage/ejs/sidebars/newIngredient.ejs"),
+        changes: vec![
+            "CREATE INGREDIENT", "СОЗДАТЬ ИНГРИДИЕНТ",
+            "NAME", "НАЗВАНИЕ",
+            "CATEGORY", "КАТЕГОРИЯ",
+            "QUANTITY", "КОЛИЧЕСТВО",
+            "UNIT", "ЕДИНИЦА",
+            ">G<", ">Г<",
+            ">KG<", ">КГ<",
+            "<option type=\"mass\" value=\"oz\">OZ</option>", "",
+            "<option type=\"mass\" value=\"lb\">LB</option>", "",
+            ">ML<", ">МЛ<",
+            ">L<", ">Л<",
+            "<option type=\"volume\" value=\"tsp\">TSP</option>", "",
+            "<option type=\"volume\" value=\"tbsp\">TBSP</option>", "",
+            "<option type=\"volume\" value=\"ozfl\">OZ. FL</option>", "",
+            "<option type=\"volume\" value=\"cup\">CUP</option>", "",
+            "<option type=\"volume\" value=\"pt\">PT</option>", "",
+            "<option type=\"volume\" value=\"qt\">QT</option>", "",
+            "<option type=\"volume\" value=\"gal\">GAL</option>", "",
+            ">MM<", ">ММ<",
+            ">CM<", ">СМ<",
+            ">M<", ">М<",
+            "<option type=\"length\" value=\"in\">IN</option>", "",
+            "<option type=\"length\" value=\"ft\">FT</option>", "",
+            "BOTTLE SIZE", "РАЗМЕР БУТЫЛКИ",
+            "CREATE", "СОЗДАТЬ",
+            "<option value=\"tsp\">TSP</option>", "",
+            "<option value=\"tbsp\">TBSP</option>", "",
+            "<option value=\"ozfl\">OZ. FL</option>", "",
+            "<option value=\"cup\">CUP</option>", "",
+            "<option value=\"pt\">PT</option>", "",
+            "<option value=\"qt\">QT</option>", "",
+            "<option value=\"gal\">GAL</option>", ""
+        ]
+    };
+
+    let new_order_ejs = Change {
+        location: format!("{}{}", folder_location, "views/dashboardPage/ejs/sidebars/newOrder.ejs"),
+        changes: vec![
+            "INGREDIENTS", "ИНГРЕДИЕНТЫ",
+            "NEW ORDER", "НОВЫЙ ЗАКАЗ",
+            "TAXES", "НАЛОГИ",
+            "OTHER FEES", "ТАРИФЫ",
+            "CREATE", "СОЗДАТЬ",
+            "REMOVE", "УДАЛИТЬ"
+        ]
+    };
+
+    let new_recipe_ejs = Change {
+        location: format!("{}{}", folder_location, "views/dashboardPage/ejs/sidebars/newRecipe.ejs"),
+        changes: vec![
+            "NEW RECIPE", "НОВЫЙ РЕЦЕПТ",
+            "NAME", "НАЗВАНИЕ",
+            "# OF INGREDIENTS", "№ ИНГРЕДИЕНТОВ",
+            "INGREDIENTS", "ИНГРЕДИЕНТОВ",
+            "INGREDIENT", "ИНГРЕДИЕНТ",
+            "QUANTITY", "КОЛИЧЕСТВО",
+            "CREATE", "СОЗДАТЬ"
+        ]
+    };
+
+    let new_transaction_ejs = Change {
+        location: format!("{}{}", folder_location, "views/dashboardPage/ejs/sidebars/newTransaction.ejs"),
+        changes: vec![
+            "NEW TRANSACTION", "НОВАЯ ТРАНЗАКЦИЯ",
+            "CREATE", "СОЗДАТЬ"
+        ]
+    };
+
+    let order_details_ejs = Change {
+        location: format!("{}{}", folder_location, "views/dashboardPage/ejs/sidebars/orderDetails.ejs"),
+        changes: vec![
+            "GRAND TOTAL", "ОБЩИЙ ИТОГ",
+            "TOTAL", "ИТОГ",
+            "TAXES", "НАЛОГИ",
+            "FEES", "ТАРИФЫ"
+        ]
+    };
+
+    let order_filter_ejs = Change {
+        location: format!("{}{}", folder_location, "views/dashboardPage/ejs/sidebars/orderFilter.ejs"),
+        changes: vec![
+            "FILTER ORDERS", "ФИЛЬТРОВАТЬ ЗАКАЗЫ",
+            "Date Range", "ДИАПАЗОН ДАТ",
+            ">Ingredients", ">ИНГРЕДИЕНТЫ",
+            "SUBMIT", "РАЗМЕСТИТЬ"
+        ]
+    };
+
+    vec![
+        menu_ejs,
+        analytics_ejs,
+        home_ejs,
+        ingredients_ejs,
+        orders_ejs,
+        recipe_book_ejs,
+        transactions_ejs,
+        edit_ingredient_ejs,
+        edit_recipe_ejs,
+        ingredient_details_ejs,
+        new_ingredient_ejs,
+        new_order_ejs,
+        new_recipe_ejs,
+        new_transaction_ejs,
+        order_details_ejs,
+        order_filter_ejs
+    ]
+}
--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,33 @@
 use std::fs::File;
 use std::io::prelude::*;
 
+mod ejs;
+use ejs::Change;
+
 fn main() {
-    let folder_location: &str = "../../javascript/InventoryManagement";
+    let files: Vec<Change> = ejs::change();
 
-    let mut file_location: String = format!("{}{}", folder_location, "/views/dashboardPage/dashboard.ejs");
-    dashboard_ejs(&file_location);
+    for i in 0..files.len() {
+        let mut contents = match read_file(&files[i].location) {
+            Ok(contents) => contents,
+            Err(e) => panic!(e)
+        };
 
-    file_location = format!("{}{}", folder_location, "/views/dashboardPage/sidebars/editIngredient.ejs");
-    edit_ingredient_ejs(&file_location);
+        let mut j = 0;
+        while j < files[i].changes.len() {
+            contents = contents.replace(files[i].changes[j], files[i].changes[j+1]);
+            j += 2;
+        }
 
-    file_location = format!("{}{}", folder_location, "/views/dashboardPage/sidebars/editRecipe.ejs");
-    edit_recipe_ejs(&file_location);
-
-    file_location = format!("{}{}", folder_location, "/views/dashboardPage/sidebars/ingredientDetails.ejs");
-    ingredient_details_ejs(&file_location);
-
-    file_location = format!("{}{}", folder_location, "/views/dashboardPage/sidebars/newIngredient.ejs");
-    new_ingredient_ejs(&file_location);
-
-    file_location = format!("{}{}", folder_location, "/views/dashboardPage/sidebars/newOrder.ejs");
-    new_order_ejs(&file_location);
-
-    file_location = format!("{}{}", folder_location, "/views/dashboardPage/sidebars/newRecipe.ejs");
-    new_recipe_ejs(&file_location);
-
-    file_location = format!("{}{}", folder_location, "/views/dashboardPage/sidebars/newTransaction.ejs");
-    new_transaction_ejs(&file_location);
-
-    file_location = format!("{}{}", folder_location, "/views/dashboardPage/sidebars/orderDetails.ejs");
-    order_details_ejs(&file_location);
-
-    file_location = format!("{}{}", folder_location, "/views/dashboardPage/sidebars/orderFilter.ejs");
-    order_filter_ejs(&file_location);
+        match write_file(&files[i].location, contents) {
+            Err(e) => panic!(e),
+            _ => ()
+        };
+    }
 }
 
 fn read_file(file: &str) -> std::io::Result<String> {
+    println!("{}", file);
     let mut file = File::open(file)?;
     let mut contents = String::new();
     file.read_to_string(&mut contents)?;
@@ -46,306 +38,4 @@ fn write_file(file: &str, contents: String) -> std::io::Result<()> {
     let mut file = File::create(file)?;
     file.write(contents.as_bytes())?;
     Ok(())
-}
-
-fn dashboard_ejs(file: &str) {
-    let mut contents = match read_file(file) {
-        Ok(contents) => contents,
-        Err(e) => panic!(e)
-    };
-
-    // TODO: get translations for the the cards on the analytics page
-    let changes = [
-        "DASHBOARD", "ГЛАВНАЯ",
-        "INGREDIENTS", "ИНГРЕДИЕНТЫ",
-        "RECIPE BOOK", "КНИГА РЕЦЕПТОВ",
-        "ANALYTICS", "АНАЛИТИКА",
-        "ORDERS", "ЗАКАЗЫ",
-        "TRANSACTIONS", "ТРАНЗАКЦИИ",
-        "LOGOUT", "ВЫХОД",
-        "INGREDIENT INVENTORY", "ИНГРЕДИЕНТЫ",
-        "Total Revenue (month)", "Общий доход (месяц)",
-        "Inventory Check", "Проверить Ингредиеты",
-        ">Update", ">ОБНОВИТЬ",
-        "UPDATE", "ОБНОВИТЬ",
-        "NEW", "НОВЫЙ",
-        "RECIPES", "РЕЦЕПТЫ",
-        "Date Range:", "Диапазон Дат:",
-        "SUBMIT", "РАЗМЕСТИТЬ",
-        "FILTER", "ФИЛЬТРОВАТЬ"
-    ];
-
-    let mut i = 0;
-    while i < changes.len() {
-        contents = contents.replace(changes[i], changes[i+1]);
-        i += 2;
-    }
-    
-    match write_file(file, contents) {
-        Err(e) => panic!(e),
-        _ => ()
-    };
-}
-
-fn edit_ingredient_ejs(file: &String) {
-    let mut contents = match read_file(file) {
-        Ok(contents) => contents,
-        Err(e) => panic!(e)
-    };
-
-    let changes = [
-        "NAME", "НАЗВАНИЕ",
-        "CATEGORY", "КАТЕГОРИЯ",
-        "MEASUREMENT UNIT", "ЕДИНИЦА ИЗМЕРЕНИЯ",
-        "SUBMIT", "РАЗМЕСТИТЬ"
-    ];
-
-    let mut i = 0;
-    while i < changes.len() {
-        contents = contents.replace(changes[i], changes[i+1]);
-        i += 2;
-    }
-
-    match write_file(file, contents) {
-        Err(e) => panic!(e),
-        _ => ()
-    };
-}
-
-fn edit_recipe_ejs(file: &str) {
-    let mut contents = match read_file(file) {
-        Ok(contents) => contents,
-        Err(e) => panic!(e)
-    };
-
-    let changes = [
-        "NAME", "НАЗВАНИЕ",
-        "INGREDIENTS", "ИНГРЕДИЕНТЫ",
-        "PRICE", "ЦЕНА",
-        "UPDATE", "ОБНОВИТЬ",
-        "CANCEL", "ОТМЕНИТЬ"
-    ];
-
-    let mut i = 0;
-    while i < changes.len() {
-        contents = contents.replace(changes[i], changes[i+1]);
-        i += 2;
-    }
-
-    match write_file(file, contents) {
-        Err(e) => panic!(e),
-        _ => ()
-    };
-}
-
-fn ingredient_details_ejs(file: &str) {
-    let mut contents = match read_file(file) {
-        Ok(contents) => contents,
-        Err(e) => panic!(e)
-    };
-
-    // TODO: translate "Average daily use"
-    let changes = [
-        "CURRENT STOCK", "ТЕКУЩИЙ ЗАПАС",
-        "RECIPES", "РЕЦЕПТЫ"
-    ];
-
-    let mut i = 0;
-    while i < changes.len() {
-        contents = contents.replace(changes[i], changes[i+1]);
-        i += 2;
-    }
-
-    match write_file(file, contents) {
-        Err(e) => panic!(e),
-        _ => ()
-    };
-}
-
-fn new_ingredient_ejs(file: &str) {
-    let mut contents = match read_file(file) {
-        Ok(contents) => contents,
-        Err(e) => panic!(e)
-    };
-
-    // TODO: need good translation for "EACH"
-    // TODO: need transaction for spreadhseet upload link
-    let changes = [
-        "CREATE INGREDIENT", "СОЗДАТЬ ИНГРИДИЕНТ",
-        "NAME", "НАЗВАНИЕ",
-        "CATEGORY", "КАТЕГОРИЯ",
-        "QUANTITY", "КОЛИЧЕСТВО",
-        "UNIT", "ЕДИНИЦА",
-        ">G<", ">Г<",
-        ">KG<", ">КГ<",
-        "<option type=\"mass\" value=\"oz\">OZ</option>", "",
-        "<option type=\"mass\" value=\"lb\">LB</option>", "",
-        ">ML<", ">МЛ<",
-        ">L<", ">Л<",
-        "<option type=\"volume\" value=\"tsp\">TSP</option>", "",
-        "<option type=\"volume\" value=\"tbsp\">TBSP</option>", "",
-        "<option type=\"volume\" value=\"ozfl\">OZ. FL</option>", "",
-        "<option type=\"volume\" value=\"cup\">CUP</option>", "",
-        "<option type=\"volume\" value=\"pt\">PT</option>", "",
-        "<option type=\"volume\" value=\"qt\">QT</option>", "",
-        "<option type=\"volume\" value=\"gal\">GAL</option>", "",
-        ">MM<", ">ММ<",
-        ">CM<", ">СМ<",
-        ">M<", ">М<",
-        "<option type=\"length\" value=\"in\">IN</option>", "",
-        "<option type=\"length\" value=\"ft\">FT</option>", "",
-        "BOTTLE SIZE", "РАЗМЕР БУТЫЛКИ",
-        "CREATE", "СОЗДАТЬ",
-        "<option value=\"tsp\">TSP</option>", "",
-        "<option value=\"tbsp\">TBSP</option>", "",
-        "<option value=\"ozfl\">OZ. FL</option>", "",
-        "<option value=\"cup\">CUP</option>", "",
-        "<option value=\"pt\">PT</option>", "",
-        "<option value=\"qt\">QT</option>", "",
-        "<option value=\"gal\">GAL</option>", ""
-    ];
-    
-    let mut i = 0;
-    while i < changes.len() {
-        contents = contents.replace(changes[i], changes[i+1]);
-        i += 2;
-    }
-
-    match write_file(file, contents) {
-        Err(e) => panic!(e),
-        _ => ()
-    };
-}
-
-fn new_order_ejs(file: &str) {
-    let mut contents = match read_file(file) {
-        Ok(contents) => contents,
-        Err(e) => panic!(e)
-    };
-
-    // TODO: need translation for name/id
-    //       need translation for spreadsheet option
-    let changes = [
-        "INGREDIENTS", "ИНГРЕДИЕНТЫ",
-        "NEW ORDER", "НОВЫЙ ЗАКАЗ",
-        "TAXES", "НАЛОГИ",
-        "OTHER FEES", "ТАРИФЫ",
-        "CREATE", "СОЗДАТЬ",
-        "REMOVE", "УДАЛИТЬ"
-    ];
-
-    let mut i = 0;
-    while i < changes.len() {
-        contents = contents.replace(changes[i], changes[i+1]);
-        i += 2;
-    }
-
-    match write_file(file, contents) {
-        Err(e) => panic!(e),
-        _ => ()
-    };
-}
-
-fn new_recipe_ejs(file: &str) {
-    let mut contents = match read_file(file) {
-        Ok(contents) => contents,
-        Err(e) => panic!(e)
-    };
-
-    // TODO: need translation for spreadsheet option
-    let changes = [
-        "NEW RECIPE", "НОВЫЙ РЕЦЕПТ",
-        "NAME", "НАЗВАНИЕ",
-        "# OF INGREDIENTS", "№ ИНГРЕДИЕНТОВ",
-        "INGREDIENTS", "ИНГРЕДИЕНТОВ",
-        "INGREDIENT", "ИНГРЕДИЕНТ",
-        "QUANTITY", "КОЛИЧЕСТВО",
-        "CREATE", "СОЗДАТЬ"
-    ];
-
-    let mut i = 0;
-    while i < changes.len() {
-        contents = contents.replace(changes[i], changes[i+1]);
-        i += 2;
-    }
-
-    match write_file(file, contents) {
-        Err(e) => panic!(e),
-        _ => ()
-    };
-}
-
-fn new_transaction_ejs(file: &str) {
-    let mut contents = match read_file(file) {
-        Ok(contents) => contents,
-        Err(e) => panic!(e)
-    };
-
-    // TODO: need translation for spreadhseet option
-    let changes = [
-        "NEW TRANSACTION", "НОВАЯ ТРАНЗАКЦИЯ",
-        "CREATE", "СОЗДАТЬ"
-    ];
-
-    let mut i = 0;
-    while i < changes.len() {
-        contents = contents.replace(changes[i], changes[i+1]);
-        i += 2;
-    }
-
-    match write_file(file, contents) {
-        Err(e) => panic!(e),
-        _ => ()
-    };
-}
-
-fn order_details_ejs(file: &str){
-    let mut contents = match read_file(file) {
-        Ok(contents) => contents,
-        Err(e) => panic!(e)
-    };
-
-    // TODO: check on total/grand total translations
-    let changes = [
-        "GRAND TOTAL", "ОБЩИЙ ИТОГ",
-        "TOTAL", "ИТОГ",
-        "TAXES", "НАЛОГИ",
-        "FEES", "ТАРИФЫ"
-    ];
-
-    let mut i = 0;
-    while i < changes.len() {
-        contents = contents.replace(changes[i], changes[i+1]);
-        i += 2;
-    }
-
-    match write_file(file, contents) {
-        Err(e) => panic!(e),
-        _ => ()
-    };
-}
-
-fn order_filter_ejs(file: &str) {
-    let mut contents = match read_file(file) {
-        Ok(contents) => contents,
-        Err(e) => panic!(e)
-    };
-
-    let changes = [
-        "FILTER ORDERS", "ФИЛЬТРОВАТЬ ЗАКАЗЫ",
-        "Date Range", "ДИАПАЗОН ДАТ",
-        ">Ingredients", ">ИНГРЕДИЕНТЫ",
-        "SUBMIT", "РАЗМЕСТИТЬ"
-    ];
-
-    let mut i = 0;
-    while i < changes.len() {
-        contents = contents.replace(changes[i], changes[i+1]);
-        i += 2;
-    }
-
-    match write_file(file, contents) {
-        Err(e) => panic!(e),
-        _ => ()
-    };
 }
