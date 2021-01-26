30000b3ca3a11d327aafbed599e827a47597d049
Lee Morgan <Morgan.LeeR@protonmail.com>
2021-01-26 11:26:41 +0300

    Update the emailing information.

--- src/controllers.rs
+++ src/controllers.rs
@@ -16,7 +16,28 @@ pub fn change<'a>() -> Vec<Change<'a>> {
         ]
     };
 
+    let email_verification_js = Change {
+        location: format!("{}{}", folder_location, "controllers/emailVerification.js"),
+        changes: vec![
+            "MG_SUBLINE_APIKEY", "MG_PERSONAL_APIKEY",
+            "mail.thesubline.net", "mg.sanoinventarium.com",
+            "<clientsupport@thesubline.net>", "<no-reply@sanoinventarium.com>",
+            "clientsupport@mail.thesubline.com", "no-reply@mg.sanoinventarium.com"
+        ]
+    };
+
+    let password_reset_js = Change {
+        location: format!("{}{}", folder_location, "controllers/passwordReset.js"),
+        changes: vec![
+            "MG_SUBLINE_APIKEY", "MG_PERSONAL_APIKEY",
+            "mail.thesubline.net", "mg.sanoinventarium.com",
+            "<clientsupport@thesubline.net>", "<no-reply@sanoinventarium.com>"
+        ]
+    };
+
     vec![
-        helper_js
+        helper_js,
+        email_verification_js,
+        password_reset_js
     ]
 }
